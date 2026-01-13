8c33d13959d95a6e851bbd70856a19e1297be65f
rayc <cr_fun@qq.com>
2026-01-13 09:32:31 +0800

    update content

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,9 @@ import HelloWorld from './components/HelloWorld.vue';
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    Uploading new content! 有一次修改,有一次修改
+    Uploading new content! 有一次修改,有一次修改，还在修改
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
